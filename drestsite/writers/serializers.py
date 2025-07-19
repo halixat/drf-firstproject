be7c50098d04fc15524ff7ed2e14c9ddd7cd1a1f
Nabi <alhoresme@gmail.com>
2025-07-19 21:47:25 +0300

    Упростить сериализатор WritersSerializer заменой базового класса Serializer на ModelSerializer

--- drestsite/writers/serializers.py
+++ drestsite/writers/serializers.py
@@ -5,33 +5,18 @@ from .models import Writers
 
 
 
+class WritersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Writers
+        fields = "__all__"
+
+
+
+
 # class WritersModel():
 #     def __init__(self, title, content):
 #         self.title = title
 #         self.content = content
-
-
-class WritersSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    content = serializers.CharField()
-    time_create = serializers.DateTimeField(read_only=True)
-    time_update = serializers.DateTimeField(read_only=True)
-    is_published = serializers.BooleanField(default=True)
-    cat_id = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return Writers.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.content = validated_data.get("content", instance.content)
-        instance.time_create = validated_data.get("time_create", instance.time_create)
-        instance.time_update = validated_data.get("time_update", instance.time_update)
-        instance.is_published = validated_data.get("is_published", instance.is_published)
-        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
-        instance.save()
-        return instance
-
 
 
 # def encode():
