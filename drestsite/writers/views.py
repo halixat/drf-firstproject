ee1360f789a84d204a7b7a3a3163214a67c35910
Nabi <alhoresme@gmail.com>
2025-07-21 00:44:51 +0300

    Добавить новый маршурут использованием декоратора action в классе WritersViewSet

--- drestsite/writers/views.py
+++ drestsite/writers/views.py
@@ -1,17 +1,23 @@
 from rest_framework import generics, viewsets
 from django.shortcuts import render
 from django.forms import model_to_dict
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Writers
+from .models import Writers, Category
 from .serializers import WritersSerializer
 
 
 
 class WritersViewSet(viewsets.ModelViewSet):
-    queryset = Writers.objects.all()
+    queryset = Writers.objects.all()[0:3]
     serializer_class = WritersSerializer
+
+    @action(methods=['get'], detail = True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 
 
